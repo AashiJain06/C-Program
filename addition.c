3dcb8a58ec9e7c1e22a0d22b0f90cc129af66c3c
Aashi Jain <aashijain064@gmail.com>
2024-07-12 12:19:01 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+
+  /*int a,b;
+  printf("enter two numbers");
+ scanf("%d%d",&a,&b);
+  int c=a+b;
+  printf("additin=%d",c);
+  return 0;*/
+  
+
+    int a; 
+    float b;
+    printf("enter one integer and decimal number");
+    scanf("%d%f",&a,&b);
+    float c=a+b;
+    printf("addition=%f",c);
+    return 0;
+
+}
+
+  
+
+
+    
+    
+   /* int a; 
+    float b;
+    printf("\nenter one integer and decimal number");
+    scanf("%d%f",&a,&b);
+    char c='a+b';
+    printf("addition=%c",c);
+    return 0;
+    } */
