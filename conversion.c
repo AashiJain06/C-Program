f28bd67d07359afe33404602721dc018be4b2ebd
Aashi Jain <aashijain064@gmail.com>
2025-07-01 23:10:52 +0530

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+
+{
+    int c,f;
+    printf("enter temperature in f");
+    scanf("%d",&f);
+    c= (f-32)*5/9;
+    printf("temperature in celsius=%d",c);
+    return 1;
+
+    
+    }
+
+
+   /* {
+        int c,k;
+        printf("enter temperature in celsius");
+        scanf("%d",&c);
+        k=c+273;
+        printf("temperatue in kelvin =%d",k);
+        return 0;*/
+
+
+    
+
+    
