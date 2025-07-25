4a227b8e62fb97deac5d8a38f37acc52ca2fb145
Aashi Jain <aashijain064@gmail.com>
2025-07-25 22:43:12 +0530

    Add files via upload

---
+++ while.c
@@ -0,0 +1,37 @@
+/*#include<stdio.h>
+int main()
+
+{
+    int n,i=1;
+    printf("\nenter number");
+    scanf("%d",&n);
+
+    while(i<=n)
+    {
+        printf("\n%d",i);
+        i++;
+
+    }
+    return 0;
+}*/
+
+#include <stdio.h>
+int main()
+
+{
+    int n, i;
+    printf("\nenter munber");
+    scanf("%d", &n);
+
+    while (i <= n)
+        ;
+    i = 1;
+    {
+        if (i % 2 == 0)
+            ;
+        printf("\n%d", i);
+        i = i + 1;
+    }
+
+    return 0;
+}
