cf25311691ace902c8411a81b940f14c2e70d6ec
Aashi Jain <aashijain064@gmail.com>
2025-07-25 22:42:45 +0530

    Add files via upload

---
+++ assignmentoperator.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+/*{
+    int a,b;
+    a=5;
+    b=8;
+    a+=b;
+    printf("\nvalue of a=%d",a);
+    a-=b;
+    printf("\nvalue of a=%d",a);
+    a*=b;
+    printf("\nva;ue of a=%d",a);
+    a/=b;
+    printf("\nvalue of a=%d",a);
+    a+=5;
+    printf("\nvalue of a=%d",a);
+    a-=7;
+    printf("\nvalue of a=%d",a);
+    return 0;
+}*/
+
+
+
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    a+=b;
+    printf("\nvalue of a=%d",a);
+    a-=b;
+    printf("\nvalue of a=%d",a);
+    a*=b;
+    printf("\nva;ue of a=%d",a);
+    a/=b;
+    printf("\nvalue of a=%d",a);
+    a+=5;
+    printf("\nvalue of a=%d",a);
+    a-=7;
+    printf("\nvalue of a=%d",a);
+    return 0;
+}
