e6ee8eb930f0eafff1ba48fe2297365718a9911d
Aashi Jain <aashijain064@gmail.com>
2025-08-18 21:49:15 +0530

    Add files via upload

---
+++ goto.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+
+
+/*{
+    int n;
+    printf("\nenter the value of n");
+    scanf("%d",&n);
+    if(n%2==0)
+    goto even;
+    else
+    goto odd;
+
+    even:
+    printf("\n number is even");
+    return 0;
+
+    odd:
+    printf("\n number is odd");
+    return 0;
+
+
+
+
+
+}*/
+
+
+
+{
+    int n,i=1;
+    printf("\n enter the value of n");
+    scanf("%d",&n);
+
+    printnum:
+    printf("\n%d",i);
+    i=i+1;
+    if(i<=20);
+    goto printnum;
+
+    return 0;
+
+}
+
